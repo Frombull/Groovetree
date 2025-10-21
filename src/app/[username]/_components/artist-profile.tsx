303aa63455f79c886b92418b9bfd1c3f5510347d
João Gabriel Costa <joaogabrielcosta13@icloud.com>
2025-10-21 14:57:38 -0300

    feat: verified badge

--- src/app/[username]/_components/artist-profile.tsx
+++ src/app/[username]/_components/artist-profile.tsx
@@ -5,6 +5,8 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/app/components/ui/avatar";
+import { BadgeCheck } from "lucide-react";
+import Image from "next/image";
 
 export function ArtistProfile({
   name,
@@ -27,12 +29,21 @@ export function ArtistProfile({
           {name && name.slice(0, 2).toUpperCase()}
         </AvatarFallback>
       </Avatar>
-      <h1
-        className="mt-6 font-sans text-4xl font-bold tracking-tight sm:text-5xl text-balance"
-        style={{ color: textColor }}
-      >
-        {title}
-      </h1>
+      <div className="flex justify-center items-center gap-2 mt-6">
+        <h1
+          className="font-sans text-4xl font-bold tracking-tight sm:text-5xl text-balance"
+          style={{ color: textColor }}
+        >
+          {title}
+        </h1>
+        <Image
+          src="/verified-icon.png"
+          alt="Badge Check"
+          className="pt-2"
+          width={32}
+          height={32}
+        />
+      </div>
       {bio && (
         <p
           className="mx-auto mt-6 max-w-md text-sm text-pretty"
